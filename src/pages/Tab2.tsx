d11f8e92d5a288e5ecdb1474e49515d80871095c
jbernste <52841665+jbernste@users.noreply.github.com>
2020-08-01 15:19:51 -0700

    commit2
    added titles to tabs and text to tab 2

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonCard, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab2.css';
 
@@ -8,16 +8,22 @@ const Tab2: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 2</IonTitle>
+          <IonTitle>PLAY AGAIN</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent>
+
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 2</IonTitle>
+            <IonTitle size="large">Luv Me Valentine Candy Sweet Words...</IonTitle>
           </IonToolbar>
         </IonHeader>
         <ExploreContainer name="Tab 2 page" />
+
+        <h2>Keep close to Nature's heart... and break clear away, once in awhile,
+        and climb a mountain or spend a week in the woods. Wash your spirit clean.
+     </h2>
+       
       </IonContent>
     </IonPage>
   );
--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -8,7 +8,7 @@ const Tab3: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 3</IonTitle>
+          <IonTitle>ABOUT LUV ME</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent>
